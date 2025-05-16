eec5d4474ba41a65078c6b190779d31610c541b3
DismissedLight <1686188646@qq.com>
2025-05-16 22:32:07 +0800

    [skip ci] code style

--- src/UnlockerIsland/Snap.Hutao.UnlockerIsland/exports.cpp
+++ src/UnlockerIsland/Snap.Hutao.UnlockerIsland/exports.cpp
@@ -6,19 +6,19 @@ static LRESULT WINAPI IslandGetWindowHookImpl(int code, WPARAM wParam, LPARAM lP
     return CallNextHookEx(NULL, code, wParam, lParam);
 }
 
-ISLAND_API HRESULT WINAPI IslandGetWindowHook(_Out_ HOOKPROC* pHookProc)
+HRESULT IslandGetWindowHook(_Out_ HOOKPROC* pHookProc)
 {
     *pHookProc = IslandGetWindowHookImpl;
     return S_OK;
 }
 
-ISLAND_API HRESULT WINAPI DllGetWindowHookForHutao(_Out_ HOOKPROC* pHookProc)
+HRESULT DllGetWindowHookForHutao(_Out_ HOOKPROC* pHookProc)
 {
     *pHookProc = IslandGetWindowHookImpl;
     return S_OK;
 }
 
-ISLAND_API HRESULT WINAPI IslandGetFunctionOffsetsSize(_Out_ UINT64* pCount)
+HRESULT IslandGetFunctionOffsetsSize(_Out_ UINT64* pCount)
 {
     *pCount = sizeof(Snap::Hutao::UnlockerIsland::FunctionOffsets);
     return S_OK;
--- src/UnlockerIsland/Snap.Hutao.UnlockerIsland/exports.h
+++ src/UnlockerIsland/Snap.Hutao.UnlockerIsland/exports.h
@@ -2,6 +2,8 @@
 
 #include "framework.h"
 
+ISLAND_API HRESULT WINAPI DllGetWindowHookForHutao(_Out_ HOOKPROC* pHookProc);
+
 ISLAND_API HRESULT WINAPI IslandGetWindowHook(_Out_ HOOKPROC* pHookProc);
 
-ISLAND_API HRESULT WINAPI DllGetWindowHookForHutao(_Out_ HOOKPROC* pHookProc);
+ISLAND_API HRESULT WINAPI IslandGetFunctionOffsetsSize(_Out_ UINT64* pCount);
