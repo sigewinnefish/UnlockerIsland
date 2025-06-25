08e962ee62572c196c901319914e3091159a6638
DismissedLight <1686188646@qq.com>
2025-06-25 15:56:32 +0800

    block non packaged call

--- src/UnlockerIsland/Snap.Hutao.UnlockerIsland/exports.cpp
+++ src/UnlockerIsland/Snap.Hutao.UnlockerIsland/exports.cpp
@@ -1,19 +1,21 @@
 #include "exports.h"
 #include "island.h"
+#include <appmodel.h>
 
 static LRESULT WINAPI IslandGetWindowHookImpl(int code, WPARAM wParam, LPARAM lParam)
 {
     return CallNextHookEx(NULL, code, wParam, lParam);
 }
 
-HRESULT IslandGetWindowHook(_Out_ HOOKPROC* pHookProc)
-{
-    *pHookProc = IslandGetWindowHookImpl;
-    return S_OK;
-}
-
 HRESULT DllGetWindowsHookForHutao(_Out_ HOOKPROC* pHookProc)
 {
+    UINT32 n{};
+    if (GetCurrentPackageFamilyName(&n, nullptr) == APPMODEL_ERROR_NO_PACKAGE)
+    {
+        *pHookProc = nullptr;
+        RETURN_WIN32(APPMODEL_ERROR_NO_PACKAGE);
+    }
+
     *pHookProc = IslandGetWindowHookImpl;
     return S_OK;
 }
